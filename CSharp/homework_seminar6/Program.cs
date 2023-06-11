c1c2a7d531ad312322ed0f1b60e7bdc8a1932ae6
KIRILL <kirilo5647@gmail.com>
2023-06-11 20:29:27 +0300

    6е домашнее задание

---
+++ CSharp/homework_seminar6/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+/*Console.WriteLine("Введите кол-во чисел в массиве");
+int num = Convert.ToInt32(Console.ReadLine());
+// int num = new Random().Next(-10,30);
+int [] array = new int [num];
+
+for (int i = 0; i <array.Length; i++)
+{
+    array[i]= new Random().Next(-10,8);
+    Console.Write(array[i]+" |");
+}
+int num1 = 0;
+for (int i = 0; i <array.Length; i++)
+{
+    if (array[i]>0)
+    {
+        num1++; 
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество положительных чисел в массиве = {num1}");
+*/
+
+
+
+
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Задайте коориднаты b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Задайте коориднаты k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Задайте коориднаты b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine("Задайте коориднаты k1");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($"Точка пересечения двух прямых: [{x},{y}]");
